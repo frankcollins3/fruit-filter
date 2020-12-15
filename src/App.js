1133994f8d4608422a87cf0d7e6859413f427aa8
frankcollins3 <fwc3rd@gmail.com>
2020-12-14 19:51:21 -0500

    [fruit-filter] add fruit props exercise

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import FruitsContainer from './FruitsContainer';
 
 function App() {
-  const fruits = ['banana', 'watermelon', 'apple', 'orange']
+  const fruits = ['banana', 'watermelon', 'apple', 'orange', 'dragonfruit']
   return (
     <div>
       <FruitsContainer fruits={fruits}/>
--- src/components/FruitContainer.js
+++ src/components/FruitContainer.js
@@ -7,43 +7,42 @@ import Input from './Input';
 class FruitContainer extends Component {
     constructor(props) {
         super()
+
         this.state = {
             // initialize the fruit list to the full list passed into props
-            fruitsToDisplay: this.props.fruits,
+            fruitsToDisplay: props.fruits,
             // initialize the filter value to empty string
             filterValue: ''
-        };
+        };      
     }
-//if you're dealing with inputs and the input has values then the parameters will be event
+
     handleFilterChange = (e) => {
         e.preventDefault();
         let filterValue = e.target.value;
-        //remove the fruits that don't contain any filter value
+        // remove fruits that don't container filter value
         const filteredFruitList = this.props.fruits.filter(fruit => {
-            // return fruit.toLowerCase().includes(filterValue.toLowerCase());
-
-        //     if (fruit.toLowerCase() === filterValue.toLowercase()) {
-        //         return true; 
-        //     }
+            return fruit.toLowerCase().includes(filterValue.toLowerCase())
         })
-
-        this.setState({ fruitsToDisplay: filteredFruitList })
+        this.setState({ fruitsToDisplay: filteredFruitList, filterValue })
     }
 
     render() {
+        // console.logs must go above returns
+        // Inside of Input Component, to access props, I will say props.value
+        // Inside of Input Component, to access props, I will say props.fruits
+        console.log('---- state ----')
         console.log(this.state.fruitsToDisplay);
-        console.log('---- props ----');
+        console.log('---- props ----')
         console.log(this.props.fruits);
-        // inside of input component, to access props, I will say props.value
-        // inside of list component, to access props, I will say props.fruits
         return (
             <div>
-               <Input value={this.state.filterValue} onChange={this.handleFilterChange}/>
-               <List fruits={this.state.fruitsToDisplay}/>
-               
+                <Input value={this.state.filterValue} onChange={this.handleFilterChange}/>
+                <List fruits={this.state.fruitsToDisplay}/>
             </div>
         )
     }
 }
 
-export default FruitContainer;
+
+
+export default FruitContainer
